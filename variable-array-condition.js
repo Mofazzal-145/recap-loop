e17251784c9b07e98ffe80f7d4d629307297f89d
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-27 23:38:40 +0600

    added first file

---
+++ variable-array-condition.js
@@ -0,0 +1,22 @@
+// variables
+var bottleColor = 'yellow';
+var waterQuantity = 1;
+var isFull = false;
+
+// Array
+var items = ['jug','mug','lamb','mobile','calendar'];
+items.indexOf('wire'); //-1
+items.push('pen');
+items.push('marker');
+items.pop('mobile');
+//conditionals
+if(itemsLenth >= 4){
+    console.log('You have too many elements on your table');
+}
+else if(itemsLenth == 4){
+    console.log('you only have four items');
+}
+else{
+    console.log('you cleaned your desh');
+}
+
