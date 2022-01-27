76055e115287010e1db4e45733916885c1d0a71f
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-28 02:11:28 +0600

    array loop file added

---
+++ array-loop.js
@@ -0,0 +1,12 @@
+var numbers = [11, 32, 43, 56, 76, 34, 12, 28, 60];
+for (var i = 0; i < numbers.length; i++){
+    var elements = numbers [i];
+    console.log(elements);
+}
+
+// new one 
+var items = ['pen','mobile','lamp','calendar','laptop','keyboard','mouse'];
+for (var i = 0; i < items.length; i++){
+    var item = items [i];
+    console.log(item);
+}
