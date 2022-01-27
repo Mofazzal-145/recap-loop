b579cdd97abc3a97cb2f6b780984ccece561781f
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-28 01:45:01 +0600

    for loop file added

---
+++ foor-loop.js
@@ -0,0 +1,17 @@
+// for ( var roastGiven = 0; roastGiven < 7; roastGiven ++){
+//     console.log('please serve the food');
+//     console.log(roastGiven);
+// }
+
+// ========
+// for (var i = 1; i <= 10; i++){
+//     console.log(i);
+// }
+// odd number
+// for (var i = 1; i <=20; i+=1){
+//     console.log(i);
+// }
+// even number
+for (var i = 0; i <=20; i+=2){
+    console.log(i);
+}
