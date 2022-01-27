efed8c16ca7916615e1096ed885a862c4c5a74db
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-28 00:52:16 +0600

    add even and odd numbers

---
+++ while-loop-example.js
@@ -0,0 +1,18 @@
+var number = 1; // loop variable
+// while (number <= 20){
+//     console.log(number);
+//     number++;
+// }
+
+// odd number 
+// while (number <= 20){
+//     console.log(number);
+//     number = number + 2;
+// }
+
+// even numbers
+var number = 0;
+while (number <= 20){
+    console.log(number);
+    number = number +2;
+}
